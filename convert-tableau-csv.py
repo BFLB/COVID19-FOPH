f48b4b768b8df860469bd73e04a0d1ce8d123df3
bernhard.fluehmann <bernhard.fluehmann@realstuff.ch>
2020-04-16 18:31:28 +0200

    data update 2020-04-11

--- convert-tableau-csv.py
+++ convert-tableau-csv.py
@@ -163,7 +163,6 @@ with open(args.source, encoding='utf-16') as csvfile:
 with open(args.target, 'w', newline='', encoding='utf-8') as target_file:
     fieldnames = ['case_number', 'case_class', 'case_date', 'canton_abbr', 'gender', 'age_class', 'age', 'confirmed', 'confirmed_date', 'death', 'death_date', 'replication_date']
     writer = csv.DictWriter(target_file, fieldnames=fieldnames, lineterminator="\n")
-    print(csv.get_dialect)
     writer.writeheader()
     for row in target_rows:
         writer.writerow(row)
